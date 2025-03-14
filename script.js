ec709e43b143ca4fce93557043cf0d0be0131e16
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-14 12:26:17 +0500

    preincrement and predecrement

---
+++ script.js
@@ -0,0 +1,46 @@
+// increment
+// decrement
+// let incr = 10
+// let decr = 10
+// ++incr
+// --decr
+// console.log(incr++)
+// console.log(decr--)
+// console.log(incr)
+// console.log(decr)
+// let a =2
+// console.log(a%1);
+// console.log(5*5 === "25");
+// && and,  || or Operator
+
+// const isYoung = false
+// const isOld = true
+// console.log(isYoung && isOld)
+// const isYoung = true
+// const isOld = true
+// const isAdult = false
+// console.log(isYoung || isOld || isAdult)
+
+// const isYoung = true
+// const isOld = true
+// const isAdult = false
+// console.log(isYoung && !isOld || isAdult)
+// let a = 5
+// let b = 4
+// console.log(a + b === 9  && b + a === 9)
+// console.log( a + b && a + b)
+// let a = true,
+//     b= false
+// console.log(a === b|| b === a && a === a)
+// let a = true
+// console.log(!a)
+// let a = true,
+//     b = false
+// console.log(a === a && !b=== true )
+// let a = 2,
+//     b = 1
+// console.log(a<=2 && b>=2)
+// let a = 2
+// console.log(a**7)
+// console.log(2 + 2 * 2 + 2 >= 8);
+console.log(2%3)
